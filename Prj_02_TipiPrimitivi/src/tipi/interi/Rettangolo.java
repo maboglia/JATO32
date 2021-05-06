3cf6bcbeffa04ee4e5ef7269cb89706e32dfc1b4
maboglia <mauro.bogliaccino@gmail.com>
2021-05-06 12:50:37 +0200

    mod

---
+++ Prj_02_TipiPrimitivi/src/tipi/interi/Rettangolo.java
@@ -0,0 +1,20 @@
+package tipi.interi;
+
+public class Rettangolo {
+
+	public static void main(String[] args) {
+		
+		int base = 5;
+		int altezza = 6;
+		
+		System.out.println("Dato un rettangolo di base: " + base);
+		System.out.println("e di altezza: " + altezza);
+
+		int superficie = base * altezza;
+		
+		System.out.println("La superficie del rettangolo è: " + superficie);
+		
+
+	}
+
+}
---
+++ Prj_02_TipiPrimitivi/src/tipi/interi/SecondaClasse.java
@@ -0,0 +1,32 @@
+package tipi.interi;
+
+public class SecondaClasse {
+
+	public static void main(String[] args) {
+		
+
+		//4 tipi interi
+		
+		//dichiarazione
+		byte a = 4;
+		short b;
+		int c;
+		long d;
+		
+		//inizializzazione
+		a = 5;
+		a = 3;
+		a = -1;
+		
+		b = 5;
+		c = 5;
+		d = 5;
+		
+		//uso
+		System.out.println(a);
+		
+		
+
+	}
+
+}
